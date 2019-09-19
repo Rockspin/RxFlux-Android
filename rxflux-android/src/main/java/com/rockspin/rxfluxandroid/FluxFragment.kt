f8c4e80e61edab97b9fa2578f530108646fc965e
Rory Kelly <rory@rockspin.co.ukl>
2019-09-19 10:29:01 +0100

    Release 0.0.4

--- rxflux-android/src/main/java/com/rockspin/rxfluxandroid/FluxFragment.kt
+++ rxflux-android/src/main/java/com/rockspin/rxfluxandroid/FluxFragment.kt
@@ -1,10 +1,78 @@
 package com.rockspin.rxfluxandroid
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
-import com.rockspin.rxfluxcore.Effect
-import com.rockspin.rxfluxcore.Event
-import com.rockspin.rxfluxcore.FluxView
-import com.rockspin.rxfluxcore.State
+import androidx.lifecycle.Lifecycle
+import com.rockspin.rxfluxcore.*
+import com.uber.autodispose.android.lifecycle.autoDisposable
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import java.lang.IllegalStateException
 
-abstract class FluxFragment<T : Event, U : State, E : Effect> : Fragment(),
-    FluxView<T, U, E>
+abstract class FluxFragment<T : Event, U : State, E : Effect> : Fragment(), FluxView<T, U, E> {
+    abstract val fluxViewModel: FluxViewModel<T, U, E>
+    private val observerOn: Scheduler =
+        AndroidSchedulers.mainThread()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // start listening for state changes when the activity is created.
+        listenForStateUpdates()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        createAndDispatchResults(Lifecycle.Event.ON_PAUSE)
+        listenForEffectUpdates(Lifecycle.Event.ON_PAUSE)
+    }
+
+    override fun receivedEffect(effect: E) {}
+
+    override fun receivedNavigation(navigation: Navigation) {}
+
+    /**
+     *  Register the events this activity generates to a ResultCreator
+     *  then send the results to the dispatcher.
+     *
+     * The subscription is automatically disposed when the activity is destroyed or when
+     * [untilEvent] occurs.
+     *
+     *  @param untilEvent Optional lifecycle event when subscription will be disposed.
+     */
+    fun createAndDispatchResults(untilEvent: Lifecycle.Event? = null): Disposable {
+        val resultCreator = fluxViewModel.resultCreator
+            ?: throw IllegalStateException("no effect mapper on view model")
+        return resultCreator.dispatchResults(events).observeOn(observerOn)
+            .autoDisposable(this, untilEvent).subscribe()
+    }
+
+    /**
+     * Register this activities to listens for state updates.
+     *
+     * The subscription is automatically disposed when the activity is destroyed or when
+     * [untilEvent] occurs.
+     *
+     * @param untilEvent Optional lifecycle event when subscription will be disposed.
+     */
+    fun listenForStateUpdates(untilEvent: Lifecycle.Event? = null): Disposable =
+        fluxViewModel.store.updates.observeOn(observerOn).autoDisposable(
+            this,
+            untilEvent
+        ).subscribe(this::stateChanged)
+
+    /**
+     * Register this activities to listens for effect updates.
+     *
+     * The subscription is automatically disposed when the activity is destroyed or when
+     * [untilEvent] occurs.
+     *
+     * @param untilEvent Optional lifecycle event when subscription will be disposed.
+     */
+    fun listenForEffectUpdates(untilEvent: Lifecycle.Event? = null): Disposable {
+        val effectMapper = fluxViewModel.effectStore
+            ?: throw IllegalStateException("no effect mapper on view model")
+        return effectMapper.updates.observeOn(observerOn).autoDisposable(this, untilEvent)
+            .subscribe(this::receivedEffect)
+    }
+}
